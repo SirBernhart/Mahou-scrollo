cbe7ce1efd2808f03b27d7a9da971f0dfd45ce80
Bernardo <behornerl@gmail.com>
2020-04-10 01:49:26 -0300

    Inporting input handler and entity movement scripts

---
+++ Mahou scrollo/Assets/Scripts/EntityMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement : MonoBehaviour
+{
+    // Public variables (Balancing tools)
+    [SerializeField] private float movespeed;
+
+    // External references
+    [SerializeField] private Rigidbody2D entityRigidbody;
+
+    private void Start()
+    {
+        movespeed *= 100;
+    }
+
+    public void MoveInDirection(Vector2 direction)
+    {
+        Vector3 moveAmount = direction * movespeed * Time.deltaTime;
+        entityRigidbody.velocity = moveAmount; 
+    }
+}
---
+++ Mahou scrollo/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // External references
+    [SerializeField] private EntityMovement playerMovement;
+
+    // Internal variables
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Vector2.zero;
+
+        // Teclado
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
+
+        playerMovement.MoveInDirection(Vector2.ClampMagnitude(moveDirection, 1f));
+    }
+}
