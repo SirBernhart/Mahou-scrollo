cebae95d87fbd000920894275bb04c22d603c024
Bernardo <behornerl@gmail.com>
2020-04-15 00:10:39 -0300

    Criando classe de detecção do jogador estar no chão

---
+++ Mahou scrollo/Assets/Scripts/GroundedController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedController : MonoBehaviour
+{
+    private bool isGrounded;
+
+    [SerializeField] private float stopBeingGroundedDelay;
+    [SerializeField] private string groundTag;
+    private Coroutine delayStopBeingGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == groundTag)
+            isGrounded = true;
+
+        if(delayStopBeingGrounded != null)
+        {
+            StopCoroutine(delayStopBeingGrounded);
+            delayStopBeingGrounded = null;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(delayStopBeingGrounded != null)
+        {
+            StopCoroutine(delayStopBeingGrounded);
+        }
+
+        delayStopBeingGrounded = StartCoroutine("DelayStopBeingGrounded");
+    }
+
+    private IEnumerator DelayStopBeingGrounded()
+    {
+        yield return new WaitForSeconds(stopBeingGroundedDelay);
+        isGrounded = false;
+    }
+
+    public bool GetIsGrounded()
+    {
+        return isGrounded;
+    }
+}
