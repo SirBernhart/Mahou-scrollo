f8bf7f5947e2853f1959f181f1a3a739c8764c66
Bernardo <behornerl@gmail.com>
2020-04-28 23:44:19 -0300

    Criando script de controle da transformação

---
+++ Mahou scrollo/Assets/Scripts/TransformationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sprite;
+
+    private Color magicColor = new Color(0, 0, 255);
+    private Color normalColor = new Color(250, 0, 0);
+
+    private bool isInNormalForm = true;
+
+    public void Transform()
+    {
+        if (isInNormalForm)
+        {
+            sprite.color = magicColor;
+            isInNormalForm = false;
+        }
+        else
+        {
+            sprite.color = normalColor;
+            isInNormalForm = true;
+        }
+    }
+}
