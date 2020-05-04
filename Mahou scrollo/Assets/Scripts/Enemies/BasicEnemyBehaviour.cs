afa18d079f20295b96ead0d944d0c30b8db16ce0
Bernardo <behornerl@gmail.com>
2020-05-03 23:20:04 -0300

    Criando script de comportamento básico do inimigo

---
+++ Mahou scrollo/Assets/Scripts/Enemies/BasicEnemyBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private EntityMovement entityMovement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        entityMovement.MoveInDirection(-1);    
+    }
+}
