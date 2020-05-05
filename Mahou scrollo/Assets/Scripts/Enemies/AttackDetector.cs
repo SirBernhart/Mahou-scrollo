47b83be4627c4621f61190495640103303621b54
Bernardo <behornerl@gmail.com>
2020-05-04 23:26:59 -0300

    Criando scripts de detecção do jogador para o inimigo básico

---
+++ Mahou scrollo/Assets/Scripts/Enemies/AttackDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDetector : MonoBehaviour
+{
+    [SerializeField] private BasicEnemyBehaviour basicEnemyBehaviour;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(basicEnemyBehaviour.playerReference == null)
+            {
+                basicEnemyBehaviour.playerReference = collision.gameObject;
+            }
+
+            basicEnemyBehaviour.ChangeState(BehaviourState.Attacking);
+        }
+    }
+
+   /* private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            basicEnemyBehaviour.ChangeState(BehaviourState.Chasing);
+        }
+    }*/
+}
---
+++ Mahou scrollo/Assets/Scripts/Enemies/PlayerChaseDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChaseDetector : MonoBehaviour
+{
+    [SerializeField] private BasicEnemyBehaviour basicEnemyBehaviour;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (basicEnemyBehaviour.playerReference == null)
+            {
+                basicEnemyBehaviour.playerReference = collision.gameObject;
+            }
+
+            basicEnemyBehaviour.ChangeState(BehaviourState.Chasing);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            basicEnemyBehaviour.ChangeState(BehaviourState.Idle);
+        }
+    }
+}
