a18ee363ec02d4ef164bab1e3cd1218be7814152
Bernardo <behornerl@gmail.com>
2020-05-04 23:46:43 -0300

    Criando script para gerenciar a vida das entidades

---
+++ Mahou scrollo/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int amount;
+
+    public void ReduceHealth(int amount)
+    {
+        this.amount -= amount;
+        Debug.Log("Ouch! Current health: " + this.amount);
+
+        if(this.amount <= 0)
+        {
+            Destroy(transform.parent.gameObject);
+        } 
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        this.amount += amount;
+        Debug.Log("Whew! Current health: " + this.amount);
+    }
+
+    public int GetAmoutOfHealth()
+    {
+        return amount;
+    }
+}
