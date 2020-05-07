8807270cacd7c55b4be9f5eac6124328c13dff66
Bernardo <behornerl@gmail.com>
2020-05-06 23:48:59 -0300

    Refinando o comportamento do inimigo básico para que cada estado esteja mais isolado

--- Mahou scrollo/Assets/Scripts/Enemies/BasicEnemyBehaviour.cs
+++ Mahou scrollo/Assets/Scripts/Enemies/BasicEnemyBehaviour.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BehaviourState {Idle, Patrol, Chasing, Attacking}
+public enum BehaviourState {Idle, Patrol, Chasing, Attacking, Dying }
 
 public class BasicEnemyBehaviour : MonoBehaviour
 {
     [SerializeField] private EntityMovement entityMovement;
-    [SerializeField] private ObstacleDetector obstacleDetector;
+    [SerializeField] private PlayerChaseDetector playerChaseDetector;
+    [SerializeField] private AttackDetector attackDetector;
     [SerializeField] private EntityAttack attackController;
     private bool canChangeState = true;
 
-    public GameObject playerReference;
+    private GameObject playerReference;
+    public GameObject GetPlayerReference() { return playerReference; }
+    public void SetPlayerReference(GameObject playerReference) { this.playerReference = playerReference; }
 
     private Coroutine attacking;
 
@@ -37,6 +40,9 @@ public class BasicEnemyBehaviour : MonoBehaviour
             case BehaviourState.Patrol:
                 //Patrol();
                 break;
+            case BehaviourState.Dying:
+                Dying();
+                break;
         }
     }
 
@@ -50,6 +56,8 @@ public class BasicEnemyBehaviour : MonoBehaviour
 
     private void Chase()
     {
+       // Debug.Log("IsChasing");
+        attackDetector.gameObject.SetActive(true);
         // move in direction of player
         if(playerReference.transform.position.x >= transform.position.x)
         {
@@ -63,7 +71,7 @@ public class BasicEnemyBehaviour : MonoBehaviour
 
     private void Idle()
     {
-        // do nothing
+        
     }
 
     private void Attacking()
@@ -76,14 +84,25 @@ public class BasicEnemyBehaviour : MonoBehaviour
         }
     }
 
-    private IEnumerator DoAttack()
+    private void Dying()
     {
         canChangeState = false;
+        StopCoroutine(attacking);
+        playerChaseDetector.gameObject.SetActive(false);
+        attackDetector.gameObject.SetActive(false);
+    }
 
-        yield return new WaitForSeconds(1);
+    private IEnumerator DoAttack()
+    {
+        playerChaseDetector.gameObject.SetActive(false);
+        attackDetector.gameObject.SetActive(false);
+        canChangeState = false;
+
+        yield return new WaitForSeconds(0.8f);
         attackController.DoMeleeAttack();
-
+        yield return new WaitForSeconds(0.3f);
         canChangeState = true;
+        playerChaseDetector.gameObject.SetActive(true);
         ChangeState(BehaviourState.Idle);
         attacking = null;
     }
