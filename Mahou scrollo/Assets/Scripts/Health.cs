f9d43e2d7d23da6d47f7b7280933f3b58650118d
Bernardo <behornerl@gmail.com>
2020-05-06 23:53:18 -0300

    Adicionando corotina que faz as entidades ficarem piscando quando levam dano e que as impede de atacar

--- Mahou scrollo/Assets/Scripts/Health.cs
+++ Mahou scrollo/Assets/Scripts/Health.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int amount;
+    [SerializeField] private GameObject graphics;
+    [SerializeField] private float blinkTime;
+    [SerializeField] private EntityAttack entityAttack;
 
     public void ReduceHealth(int amount)
     {
         this.amount -= amount;
         Debug.Log("Ouch! Current health: " + this.amount);
 
+        StartCoroutine(Blink());
+
         if(this.amount <= 0)
         {
-            Destroy(transform.parent.gameObject);
+            StartCoroutine(KillEntity());
         } 
     }
 
@@ -26,5 +31,26 @@ public class Health : MonoBehaviour
     public int GetAmoutOfHealth()
     {
         return amount;
+    }
+
+    private IEnumerator Blink()
+    {
+        entityAttack.SetCanAttack(false);
+
+        for(float timer = 0 ; timer < blinkTime ; timer += Time.deltaTime)
+        {
+            graphics.SetActive(!graphics.activeInHierarchy);
+
+            yield return null;
+        }
+        graphics.SetActive(true);
+
+        entityAttack.SetCanAttack(true);
+    }
+
+    private IEnumerator KillEntity()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(transform.parent.gameObject);
     }
 }
