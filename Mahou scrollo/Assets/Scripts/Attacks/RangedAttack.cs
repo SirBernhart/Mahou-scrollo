71061ca37f4aa45c8b6b4e0ec37827cd95e2cad0
Bernardo <behornerl@gmail.com>
2020-05-29 01:46:35 -0300

    Mudando a forma que os parâmetros do ataque à distância são configurados

--- Mahou scrollo/Assets/Scripts/Attacks/RangedAttack.cs
+++ Mahou scrollo/Assets/Scripts/Attacks/RangedAttack.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class RangedAttack : AttackBase
 {
     [SerializeField] private GameObject attackProjectile;
+    [SerializeField] private float projectileSpeed;
 
     public new void Attack()
     {
         if (canAttack)
         {
-            GameObject spawnedProjectile = Instantiate(attackProjectile, transform.position, transform.rotation);
-            spawnedProjectile.GetComponentInChildren<ProjectileBehavior>().SetMoveDirection(transform.root);
-            spawnedProjectile.tag = transform.root.tag;
+        Instantiate(attackProjectile, transform.position, transform.rotation)
+                .GetComponentInChildren<ProjectileBehavior>().
+                SetProjectileProperties(GetAttackDirection(), damage, projectileSpeed, transform.root.gameObject.tag);
 
             base.Attack();
         }
+    }
+
+    private Vector2 GetAttackDirection()
+    {
+        if (transform.root.rotation.y == 0)
+            return new Vector2(1, 0);
+        else
+            return new Vector2(-1, 0);
     }
 }
