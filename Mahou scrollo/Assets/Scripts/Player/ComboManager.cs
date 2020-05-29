c9ed819d23392f06b9a6ef4ff1db6a7ec73099ef
Bernardo <behornerl@gmail.com>
2020-05-29 02:58:24 -0300

    Criando script gerenciador dos combos

---
+++ Mahou scrollo/Assets/Scripts/Player/ComboManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour
+{
+    [SerializeField] private float comboResetTime;
+    private float comboTimer = 0;
+    private Coroutine comboTimerCoroutine;
+
+    //private ComboElement[] currentCombo = new ComboElement[5];
+    private ComboElement currentElement;
+
+    public void AddToCombo(ComboElement newElement)
+    {
+        if (newElement.isFinisher)
+        {
+            ClearCombo();
+        }
+        else
+        {
+            if (comboTimerCoroutine == null)
+                StartCoroutine(StartComboResetTimer());
+
+            if (currentElement != null && CheckIfNewElementMatchesNextElements(newElement))
+            {
+                currentElement = newElement;
+            }
+            else
+            {
+                StopCoroutine(comboTimerCoroutine);
+                ClearCombo();
+            }
+            comboTimer = 0;
+        }
+    }
+
+    private bool CheckIfNewElementMatchesNextElements(ComboElement newElement)
+    {
+        for(int i = 0 ; i < currentElement.nextElements.Length ; ++i)
+        {
+            if (currentElement.nextElements[i].Equals(newElement))
+                return true;
+        }
+
+        return false;
+    }
+
+    private void ClearCombo()
+    {
+        currentElement = null;
+    }
+
+    private IEnumerator StartComboResetTimer()
+    {
+        for(; comboTimer < comboResetTime ; comboTimer += Time.deltaTime)
+        {
+            yield return null;  
+        }
+
+        comboTimer = 0;
+        ClearCombo();
+    }
+}
