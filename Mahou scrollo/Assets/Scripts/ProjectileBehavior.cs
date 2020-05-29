faf32a4fd507d65795d0ff29dc51bf9061a041df
Bernardo <behornerl@gmail.com>
2020-05-29 01:45:28 -0300

    Criando script de comportamento do projétil

---
+++ Mahou scrollo/Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D projectileRigidbody;
+    private float speed;
+    public void SetSpeed(float speed) { this.speed = speed; }
+
+    private int damageAmount;
+    public void SetDamageAmount(int damange)
+    {
+        // Can only be set to a value != 0 once
+        if(damageAmount == 0)
+        {
+            damageAmount = damange;
+        }
+    }
+
+    public void SetProjectileProperties(Vector2 direction, int damage, float speed, string tag)
+    {
+        damageAmount = damage;
+        this.speed = speed;
+        SetMoveDirection(direction);
+        gameObject.tag = tag;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Avoids shooter's projectiles colliding with himself
+        if (this.gameObject.tag != collision.tag)
+        {
+            if(collision.tag == "Enemy" || collision.tag == "Player")
+            {
+                DoDamage(collision.gameObject);
+            }
+            if (collision.tag != "Untagged")
+            {
+                Destroy(transform.root.gameObject);
+            }
+        }
+    }
+
+    public void SetMoveDirection(Vector2 direction)
+    {
+        projectileRigidbody.velocity = direction * speed;
+    }
+
+    private void DoDamage(GameObject entityToDamage)
+    {
+        Health entityHealthReference = entityToDamage.GetComponentInChildren<Health>();
+
+        // Only damages entities that have the "Health" component
+        if (entityHealthReference != null)
+        {
+            entityHealthReference.ReduceHealth(damageAmount);
+        }
+    }
+}
