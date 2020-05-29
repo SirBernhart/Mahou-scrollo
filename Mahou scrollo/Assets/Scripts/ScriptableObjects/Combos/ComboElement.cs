30dd51986277eb1c0ef4ff638a8e535daf75f728
Bernardo <behornerl@gmail.com>
2020-05-29 05:06:44 -0300

    Alterando os scriptable objects de lugar, criando o SunBolt e alterando alguns valores

--- Mahou scrollo/Assets/Scripts/ScriptableObjects/Combos/ComboElement.cs
+++ Mahou scrollo/Assets/Scripts/ScriptableObjects/Combos/ComboElement.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum SpecialEffect { none, knockback, stun }
+public enum ActionType { none, lightMelee, heavyMelee, lightRanged, heavyRanged, transformation }
 
 [CreateAssetMenu(fileName = "ComboElement", menuName = "ScriptableObjects/Combo Element")]
 public class ComboElement : ScriptableObject
 {
     public ComboElement[] nextElements;
     public SpecialEffect specialEffect;
+    public float specialEffectIntensity;
+    public ActionType actionToTrigger;
 
+    public float cooldown;
     public int damage;
     public bool isFinisher;
 }
