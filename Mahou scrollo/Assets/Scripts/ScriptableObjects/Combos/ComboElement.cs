ff1ed88ac740f999872ddbb1dbbd1fe56a99ec94
Bernardo <behornerl@gmail.com>
2020-05-29 02:57:27 -0300

    Criando Scriptable object de elemento de combo

---
+++ Mahou scrollo/Assets/Scripts/ScriptableObjects/Combos/ComboElement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpecialEffect { none, knockback, stun }
+
+[CreateAssetMenu(fileName = "ComboElement", menuName = "ScriptableObjects/Combo Element")]
+public class ComboElement : ScriptableObject
+{
+    public ComboElement[] nextElements;
+    public SpecialEffect specialEffect;
+
+    public int damage;
+    public bool isFinisher;
+}
