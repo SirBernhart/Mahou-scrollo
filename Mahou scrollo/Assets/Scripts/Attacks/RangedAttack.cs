8adbf7c382c2577b616c85a55864c8c85add8c6c
Bernardo <behornerl@gmail.com>
2020-05-29 01:01:46 -0300

    Modularizando o EntityAttack em 3 scripts diferentes: AttackBase, RangedAttack e MeleeAttack

---
+++ Mahou scrollo/Assets/Scripts/Attacks/RangedAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : AttackBase
+{
+    [SerializeField] private GameObject attackProjectile;
+
+    public new void Attack()
+    {
+        if (canAttack)
+        {
+            GameObject spawnedProjectile = Instantiate(attackProjectile, transform.position, transform.rotation);
+            spawnedProjectile.GetComponentInChildren<ProjectileBehavior>().SetMoveDirection(transform.root);
+            spawnedProjectile.tag = transform.root.tag;
+
+            base.Attack();
+        }
+    }
+}
