bc6f28ce9a318988c4449bc9d5bb9424fd74e13f
Bernardo <behornerl@gmail.com>
2020-06-12 10:46:30 -0300

    Criando script que gerencia a pontuação do jogador

---
+++ Mahou scrollo/Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private int score;
+    [SerializeField] private Text scoreText;
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
+    public void IncreaseScore(int score)
+    {
+        this.score += score;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+}
