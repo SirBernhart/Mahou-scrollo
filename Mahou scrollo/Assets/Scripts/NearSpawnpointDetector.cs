efd33910ecd3e4c3b20389de09e4d4d64ce42bbc
Bernardo <behornerl@gmail.com>
2020-06-12 10:44:49 -0300

    Criando script que bloquei o spawn caso haja alguma coisa em cima do spawn point

---
+++ Mahou scrollo/Assets/Scripts/NearSpawnpointDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearSpawnpointDetector : MonoBehaviour
+{
+    public bool SomethingIsNear { get; private set; }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        SomethingIsNear = true;    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        SomethingIsNear = false;
+    }
+}
