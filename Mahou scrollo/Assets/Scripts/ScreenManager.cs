646ea0265c0ebe87245198b21f890b9f17513878
Bernardo <behornerl@gmail.com>
2020-06-11 23:26:28 -0300

    Adicionando método que ativa o menu de pause

--- Mahou scrollo/Assets/Scripts/ScreenManager.cs
+++ Mahou scrollo/Assets/Scripts/ScreenManager.cs
@@ -6,13 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class ScreenManager : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+
     public void Restart()
     {
         SceneManager.LoadScene("SampleScene");
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetPauseMenuActive()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
+
+        if (pauseMenu.activeInHierarchy)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
     }
 }
